8e041a00fe164e8f44e2216d97d8e685f88088a5
Abhishek Kumar <abhishekkr1895@gmail.com>
2018-11-04 01:38:41 +0530

    Coding the Game

--- colorGame.js
+++ colorGame.js
@@ -7,8 +7,9 @@ var colors = [
   'rgb(255, 0, 255)'
 ];
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
 colorDisplay.textContent = pickedColor;
 for (var i = 0; i < squares.length; i++) {
   //add initial colors to squares
@@ -19,9 +20,22 @@ for (var i = 0; i < squares.length; i++) {
     var clickedColor = this.style.backgroundColor;
     //compare color of the picked color
     if (clickedColor === pickedColor) {
-      alert('Correct');
+      messageDisplay.textContent = 'Correct!';
+      changeColors(clickedColor);
     } else {
-      alert('Incorrect');
+      this.style.backgroundColor = '#232323';
+      messageDisplay.textContent = 'Try Again';
     }
   });
+}
+function changeColors(color) {
+  //loop through squares
+  for (var i = 0; i < squares.length; i++) {
+    //change each color to match given color
+    squares[i].style.backgroundColor = color;
+  }
+}
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length + 1);
+  return colors[random];
 }
