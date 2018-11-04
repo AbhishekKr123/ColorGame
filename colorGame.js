5cf2cc1032e6ac6b52b36c682b37780f8cbc1b13
Abhishek Kumar <abhishekkr1895@gmail.com>
2018-11-04 22:49:16 +0530

    Adding level of game

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,43 @@
-var colors = generateRandomColors(6); //Generate An Array of Random Colors
+var numberOfSquares = 6;
+var colors = generateRandomColors(numberOfSquares); //Generate An Array of Random Colors
 var squares = document.querySelectorAll('.square');
 var pickedColor = pickColor(); //Pick an element from the Colors Array
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
 var resetButton = document.getElementById('reset');
+var easyBtn = document.querySelector('#easyBtn');
+var hardBtn = document.querySelector('#hardBtn');
+
 colorDisplay.textContent = pickedColor;
+
+easyBtn.addEventListener('click', function() {
+  hardBtn.classList.remove('selected');
+  easyBtn.classList.add('selected');
+  numberOfSquares = 3;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    if (colors[i]) {
+      squares[i].style.backgroundColor = colors[i];
+    } else {
+      squares[i].style.display = 'none';
+    }
+  }
+});
+hardBtn.addEventListener('click', function() {
+  easyBtn.classList.remove('selected');
+  hardBtn.classList.add('selected');
+  numberOfSquares = 3;
+  colors = generateRandomColors(numberOfSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    squares[i].style.display = 'block';
+  }
+});
 
 //Selecting the elements of the array and add an event Listener to it
 for (var i = 0; i < squares.length; i++) {
@@ -31,7 +63,7 @@ for (var i = 0; i < squares.length; i++) {
 //Adding An Event On Reset Button
 resetButton.addEventListener('click', function() {
   //generate all new colors
-  colors = generateRandomColors(6);
+  colors = generateRandomColors(numberOfSquares);
   //pick a new random color  from array
   pickedColor = pickColor();
   //change colors of squares
