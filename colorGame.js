ace3e3ee5b7f2545fa01b890518b900ca69b27d8
Abhishek Kumar <abhishekkr1895@gmail.com>
2018-11-04 21:27:48 +0530

    Logic Update

--- colorGame.js
+++ colorGame.js
@@ -1,10 +1,13 @@
-var colors = generateRandomColors(6);
+var colors = generateRandomColors(6); //Generate An Array of Random Colors
 var squares = document.querySelectorAll('.square');
-var pickedColor = pickColor();
+var pickedColor = pickColor(); //Pick an element from the Colors Array
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector('#message');
 var h1 = document.querySelector('h1');
+var resetButton = document.getElementById('reset');
 colorDisplay.textContent = pickedColor;
+
+//Selecting the elements of the array and add an event Listener to it
 for (var i = 0; i < squares.length; i++) {
   //add initial colors to squares
   squares[i].style.backgroundColor = colors[i];
@@ -17,12 +20,28 @@ for (var i = 0; i < squares.length; i++) {
       messageDisplay.textContent = 'Correct!';
       changeColors(clickedColor);
       h1.style.backgroundColor = pickedColor;
+      resetButton.textContent = 'Play Again!';
     } else {
       this.style.backgroundColor = '#232323';
       messageDisplay.textContent = 'Try Again';
     }
   });
 }
+
+//Adding An Event On Reset Button
+resetButton.addEventListener('click', function() {
+  //generate all new colors
+  colors = generateRandomColors(6);
+  //pick a new random color  from array
+  pickedColor = pickColor();
+  //change colors of squares
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+  h1.style.backgroundColor = '#232323';
+});
+
 function changeColors(color) {
   //loop through squares
   for (var i = 0; i < squares.length; i++) {
@@ -31,7 +50,7 @@ function changeColors(color) {
   }
 }
 function pickColor() {
-  var random = Math.floor(Math.random() * colors.length + 1);
+  var random = Math.floor(Math.random() * colors.length);
   return colors[random];
 }
 function generateRandomColors(num) {
